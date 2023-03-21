469e3f5ba89ca39a67a950590a5c618ae5be62c0
Yesenia <1719110284@utectulancingo.edu.mx>
2023-03-20 21:04:41 -0600

    v1

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,18 +5,9 @@ const assets = [
     "/index.html",
     "/css/style.css",
     "/js/app.js",
-    "/images/20_Const1917_rev_pagMon20.png",
-    "/images/C_20pesosBelisario_144.png",
-    "/images/C_20pesosEjercito_144.png",
-    "/images/C_20pesosPazCambioMilenio_144.png",
-    "/images/C_20pesosPazNobel_144.png",
-    "/images/C_20pesosVeracruz_144.png",
-    "/images/C_20pesosZacatecas_142.png",
-    "/images/C_mon20pesos_PlanMarina.png",
-    "/images/Fuerza_Aerea_20_revTransp_peq.png",
-    "/images/Morelos_20_anvTransp_peq.png",
-    "/images/C_mon_20_pesos_PlanDNIIIE.png",
-    "/images/moneda.png",
+    "/css/main.css",
+    "/css/noscript.css",
+    "/css/noscript.scss",
 ]
 
 self.addEventListener("install", installEvent => {
